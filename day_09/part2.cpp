110e6c3596ef49c5aa35abd589f8c55819723841
A-Bergonzoli <ale_bergonzoli@libero.it>
2024-05-19 19:59:50 +0200

    [09] C++ Solutions

---
+++ day_09/part2.cpp
@@ -0,0 +1,56 @@
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "pypp.hpp"
+
+using vectors = std::vector<std::vector<int>>;
+
+constexpr auto expected { 884 };
+
+class Solution {
+public:
+    void Parse(const std::string& file_name)
+    {
+        auto lines = pypp::splitFileLines(file_name);
+        for (const auto& line : lines) {
+            std::vector<int> history_entries {};
+            auto entries_as_string = pypp::split(line, ' ');
+            for (const auto& elem : entries_as_string)
+                history_entries.push_back(std::stoi(elem));
+
+            histories_.push_back(history_entries);
+        }
+    }
+
+    int ExtrapolatePrevious(const std::vector<int>& elems)
+    {
+        // base case
+        if (std::ranges::all_of(elems, [](auto elem) { return elem == 0; }))
+            return elems[0];
+
+        std::vector<int> new_elems {};
+        for (int i = 1; i < elems.size(); ++i)
+            new_elems.push_back(elems[i] - elems[i - 1]);
+
+        return elems[0] - ExtrapolatePrevious(new_elems);
+    }
+
+    vectors histories_ {};
+};
+
+int main()
+{
+    int result { 0 };
+
+    Solution solution {};
+    solution.Parse("input.txt");
+    for (auto history : solution.histories_)
+        result += solution.ExtrapolatePrevious(history);
+
+    std::cout << "Sum of extrapolated values: " << result << "\n";
+
+    assert(expected == result);
+    return 0;
+}
